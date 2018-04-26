69aca21ae14828773bdd8976f8d25249552f9552
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-26 18:52:56 +0900

    initialize
    @

--- block.rb
+++ block.rb
@@ -1,20 +1,17 @@
 class Dog
 
-	def initialize
-		@weight = 0.5
+def initialize
+	@weight = 0.5
+end
+
+	def eat
+	@weight = @weight + 0.5	
+		puts @weight
+	end
 
 end
 
-def my_weight
-	@weight.to_s
-end
-
-def eat
-	@weight = @weight + 0.5
-
-end
-
-end
-
-
-
+bbbb = Dog.new
+bbbb.eat
+bbbb.eat
+bbbb.eat
